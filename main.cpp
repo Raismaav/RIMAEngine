666f4b9140ab6b94ffb411d4dda9be4bee417496
Rafael Magaña <raismaav.2510@gmail.com>
2024-05-15 20:58:01 -0600

    Graphic library implemented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "RIMA Engine");
+    sf::CircleShape shape(50);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
