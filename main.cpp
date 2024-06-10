8935ddeb53c1b8642c0297f2285cd68ff453d6ce
Rafael Magaña <raismaav.2510@gmail.com>
2024-06-09 22:45:46 -0600

    New figures

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ int main()
     shape2.setFillColor(sf::Color::Red);
     sf::CircleShape shape3(5);
     shape2.setFillColor(sf::Color::Blue);
+    sf::CircleShape shape4(50);
+    shape.setFillColor(sf::Color::Green);
+    sf::CircleShape shape5(10);
+    shape2.setFillColor(sf::Color::Red);
+    sf::CircleShape shape6(5);
+    shape2.setFillColor(sf::Color::Blue);
 
     while (window.isOpen())
     {
@@ -24,6 +30,9 @@ int main()
         window.draw(shape);
         window.draw(shape2);
         window.draw(shape3);
+        window.draw(shape4);
+        window.draw(shape5);
+        window.draw(shape6);
         window.display();
     }
 
